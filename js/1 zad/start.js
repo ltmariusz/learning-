8df174535fa8e182b28d5e17f927ce7c78223293
ltmariusz <ltmariuszl@gmail.com>
2024-07-22 20:05:42 +0200

    js training 3 first exercise

---
+++ js/1 zad/start.js
@@ -0,0 +1,28 @@
+//1. pobieramy wszystkie potrzebne elementy z DOM
+
+const btnIncrease = document.querySelector(".bigger");
+const btnDecrease = document.querySelector(".smaller");
+const text = document.querySelector("p");
+
+//1b - określamy potrzebne dane
+
+let textSize = 24; //początkowa wielkość font-size dla p
+
+text.style.fontSize = textSize + "px";
+
+//3. Określenie akcji - napisanie funkcji
+function textIncrease() {
+  textSize++;
+  text.style.fontSize = textSize + "px";
+}
+
+function textDecrease() {
+  textSize--;
+  text.style.fontSize = textSize + "px";
+}
+
+//2. Ustawienie nasłuchiwania na przyciskach na kliknięcie.
+
+btnIncrease.addEventListener("click", textIncrease);
+btnDecrease.addEventListener("click", textDecrease);
+console.log("test2");
