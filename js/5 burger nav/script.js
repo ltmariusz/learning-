42b08b06201f967dfa801983f31288abc0a56c70
ltmariusz <ltmariuszl@gmail.com>
2024-07-29 23:54:08 +0200

    exercise burger nav

---
+++ js/5 burger nav/script.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector(".burger");
+
+burger.addEventListener("click", function () {
+  burger.classList.toggle("active");
+  document.querySelector(".fa-bars").classList.toggle("active");
+  document.querySelector(".fa-xmark").classList.toggle("active");
+});
