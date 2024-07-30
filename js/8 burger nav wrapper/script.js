d5b8f6cef83a475742a3c92934167248d7523b44
ltmariusz <96800290+ltmariusz@users.noreply.github.com>
2024-07-30 12:22:03 +0000

    next burger menu exercise

---
+++ js/8 burger nav wrapper/script.js
@@ -0,0 +1,19 @@
+const burgerV2 = document.querySelector(".burger")
+const activeAllElements = document.querySelectorAll(".activeJs")
+burgerV2.addEventListener("click", function () {
+  for (let i = 0; i < activeAllElements.length; i++) {
+    const element = activeAllElements[i];
+    element.classList.toggle("active")
+    
+  }
+})
+
+
+// $(".burger").on("click", function () {
+//   $(".fa-solid, .burger, .wrapper").toggleClass("active");
+// });
+// const burger = document.querySelector(".burger");
+
+// document.querySelector(".powrot").addEventListener("click", function () {
+//   window.location.href = "../";
+// });
